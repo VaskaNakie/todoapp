0e912b247a63cc318ff11644de0d735b3f0b55bb
Vaska Nakie <naqevaska@gmail.com>
2022-04-26 20:27:54 +0300

    Updates

---
+++ precache-manifest.6065083e6aee13ca556119e5977ea009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18997e2919836043743d",
+    "url": "/VaskaNakie/todoapp/static/css/main.def9cecd.chunk.css"
+  },
+  {
+    "revision": "18997e2919836043743d",
+    "url": "/VaskaNakie/todoapp/static/js/main.18997e29.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/VaskaNakie/todoapp/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "b0a9c8493df16ed9501c",
+    "url": "/VaskaNakie/todoapp/static/js/2.b0a9c849.chunk.js"
+  },
+  {
+    "revision": "76c7b16c0ba8aa8cf7d9",
+    "url": "/VaskaNakie/todoapp/static/js/runtime~main.76c7b16c.js"
+  },
+  {
+    "revision": "2ce42053e2423f864a53ff7c7012c2fa",
+    "url": "/VaskaNakie/todoapp/index.html"
+  }
+];
